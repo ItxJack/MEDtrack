d24d1de77d870bdcd8e989ad4fbd3a3b211a3292
Rahul Ranjan <rahulranjan.rawz@gmail.com>
2026-07-22 17:06:00 +0530

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -39,7 +39,7 @@ def current_user():
                 pass
     return g.user
 
-
+#authentication
 def login_required(f):
     @wraps(f)
     def wrapped(*args, **kwargs):
@@ -48,7 +48,7 @@ def login_required(f):
         return f(*args, **kwargs)
     return wrapped
 
-
+#autorization
 def role_required(*roles):
     def decorator(f):
         @wraps(f)
